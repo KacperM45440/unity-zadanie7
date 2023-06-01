1351e68b5b9845df7221eeb31332c07b51f03159
KacperM45440 <104095161+KacperM45440@users.noreply.github.com>
2023-06-01 21:59:52 +0200

    Buttons fix

---
+++ Assets/Challenge/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+    public GameManagerX gameManagerX;
+    public int difficulty;
+    
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        gameManagerX.StartGame(difficulty);
+    }
+
+
+
+}
---
+++ Assets/Challenge/Scripts/TargetX.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetX : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameManagerX gameManagerX;
+    public int pointValue;
+    public GameObject explosionFx;
+
+    private float minValueX = -3.75f; // the x value of the center of the left-most square
+    private float minValueY = -3.75f; // the y value of the center of the bottom-most square
+    private float spaceBetweenSquares = 2.5f; // the distance between the centers of squares on the game board
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+
+        transform.position = RandomSpawnPosition();
+
+    }
+
+    // When target is clicked, destroy it, update score, and generate explosion if object is bad - GameOver
+    private void OnMouseDown()
+    {
+        if (gameManagerX.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManagerX.UpdateScore(pointValue);
+            Explode();
+        }
+               
+    }
+    
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = RandomSquareIndex() * spaceBetweenSquares;
+        float spawnPosY = RandomSquareIndex() * spaceBetweenSquares;
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
+        return spawnPosition;
+
+    }
+    
+    int RandomSquareIndex ()
+    {
+        return Random.Range(0, 4);
+    }
+
+    void Explode ()
+    {
+        Instantiate(explosionFx, transform.position, explosionFx.transform.rotation);
+    }
+    
+
+}
